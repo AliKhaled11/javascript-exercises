b435773feb7afd530198f211e157c780fc26a472
Aly Khalid <khaledhod111@gmail.com>
2025-02-03 06:57:01 +0200

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (a < 0) {
+    return "OOPS";
+  }
+  let fibList = [0, 1];
+  for (let i = 0; i <= 100; i++) {
+    fibList.push(fibList.at(-1) + fibList.at(-2));
+  }
+  return fibList[a];
 };
 
 // Do not edit below this line
