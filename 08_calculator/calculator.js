d67d782350d421119a6fc2519f1580a5b9a173ef
Aly Khalid <khaledhod111@gmail.com>
2025-02-03 06:17:32 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (...numbers) {
+  return numbers.reduce((a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (...numbers) {
+  return numbers.reduce((a, b) => {
+    return a - b;
+  });
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  } else if (a < 0) {
+    return undefined;
+  } else {
+    let arr = [];
+    for (let i = a; i >= 1; i--) {
+      arr.push(i);
+    }
+    return arr.reduce((a, b) => {
+      return a * b;
+    });
+  }
 };
 
 // Do not edit below this line
