9928b39704d357accade19803f842b507b301f3c
Aly Khalid <khaledhod111@gmail.com>
2025-02-03 20:28:56 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let arrayCopy = arr.map((person) => {
+    return {
+      ...person,
+      age: person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth
+    };
+  });
+  arrayCopy.sort((a, b) => {
+    return a.age > b.age ? -1 : 1;
+  });
+  return arrayCopy[0];
 };
 
 // Do not edit below this line
