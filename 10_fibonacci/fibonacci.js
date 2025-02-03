dfb7189c57359c9254b9b93438c065e3a73ea63c
Aly Khalid <khaledhod111@gmail.com>
2025-02-03 20:43:04 +0200

    Resolve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,20 @@
+// const fibonacci = function (a) {
+//   if (a < 0) {
+//     return "OOPS";
+//   }
+//   let fibList = [0, 1];
+//   for (let i = 0; i <= 100; i++) {
+//     fibList.push(fibList.at(-1) + fibList.at(-2));
+//   }
+//   return fibList[a];
+// };
+
 const fibonacci = function (a) {
   if (a < 0) {
     return "OOPS";
   }
   let fibList = [0, 1];
-  for (let i = 0; i <= 100; i++) {
+  for (let i = 2; i <= a; i++) {
     fibList.push(fibList.at(-1) + fibList.at(-2));
   }
   return fibList[a];
